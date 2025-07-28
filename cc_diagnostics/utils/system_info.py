314066bf69fbc6cb25db3f04b79f0afddb00baf3
ComputerConnection <128292053+ComputerConnection@users.noreply.github.com>
2025-07-27 19:46:49 -0500

    Add hostname and user info to system_info

--- cc_diagnostics/utils/system_info.py
+++ cc_diagnostics/utils/system_info.py
@@ -1,5 +1,7 @@
 import platform
 import time
+import socket
+import getpass
 import psutil
 
 
@@ -10,4 +12,6 @@ def get_system_info():
         "Cores": psutil.cpu_count(logical=True),
         "RAM_GB": round(psutil.virtual_memory().total / 1e9, 2),
         "Uptime_Hours": round((time.time() - psutil.boot_time()) / 3600, 2),
+        "Hostname": socket.gethostname(),
+        "User": getpass.getuser(),
     }
